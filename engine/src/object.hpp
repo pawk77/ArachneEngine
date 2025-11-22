a6eb354287ea942604a28f49c178bb515b59d624
Dmitry Sirachenko <dmitry.sirachenko@bestway.com.ua>
2025-11-22 18:47:32 +0200

    Introduce object factory

--- engine/src/object.hpp
+++ engine/src/object.hpp
@@ -2,14 +2,39 @@
 
 #pragma once
 
+#include <string>
+
 namespace realware
 {
+	class cContext;
+	
 	class cObject
 	{
 	public:
 		explicit cObject() = default;
 		~cObject() = default;
+	};
 
-	private:
+	class iObjectFactory
+	{
+	public:
+		explicit iObjectFactory(cContext* context) : _context(context) {}
+		~iObjectFactory() = default;
+
+		virtual cObject* Create() = 0;
+		const std::string& GetType() { return _type; }
+
+	protected:
+		cContext* _context = nullptr;
+		std::string _type = "";
+	};
+
+	template <typename T>
+	class cObjectFactory : public iObjectFactory
+	{
+	public:
+		explicit cObjectFactory(cContext* context) : iObjectFactory(context) {}
+
+		virtual T* Create() override final {  }
 	};
 }
