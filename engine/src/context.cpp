3496ba609bc7226f6d7d4258a217a4aac75365b1
Dmitry Sirachenko <dmitry.sirachenko@bestway.com.ua>
2025-11-27 20:33:01 +0200

    Add methods to cContext

--- engine/src/context.cpp
+++ engine/src/context.cpp
@@ -4,4 +4,16 @@
 
 namespace realware
 {
+	void cContext::CreateMemoryPool()
+	{
+		_memoryPool = std::make_shared<cMemoryPool>();
+	}
+
+	void cContext::RegisterSubsystem(iObject* object)
+	{
+		ClassType type = object->GetType();
+		const auto it = _subsystems.find(type);
+		if (it == _subsystems.end())
+			_subsystems.insert({ type, std::make_shared<iObject>(object) });
+	}
 }
--- engine/src/context.hpp
+++ engine/src/context.hpp
@@ -3,19 +3,54 @@
 #pragma once
 
 #include <unordered_map>
+#include "object.hpp"
 #include "types.hpp"
 
 namespace realware
 {
-	class cObject;
+	class cMemoryPool;
 
 	class cContext
 	{
+		REALWARE_CLASS(cContext)
+
 	public:
 		explicit cContext() = default;
 		~cContext() = default;
 
 		template <typename T>
-		void Create();
+		cFactoryObject* Create();
+
+		void CreateMemoryPool();
+
+		template <typename T>
+		void RegisterFactory();
+
+		void RegisterSubsystem(iObject* object);
+
+	private:
+		std::shared_ptr<cMemoryPool> _memoryPool;
+		std::unordered_map<ClassType, std::shared_ptr<iFactory>> _factories;
+		std::unordered_map<ClassType, std::shared_ptr<iObject>> _subsystems;
 	};
+
+	template <typename T>
+	cFactoryObject* cContext::Create()
+	{
+		ClassType type = T::GetType();
+		const auto it = _factories.find(type);
+		if (it != _factories.end())
+			return _factories[type]->Create();
+		else
+			return nullptr;
+	}
+
+	template <typename T>
+	void cContext::RegisterFactory()
+	{
+		ClassType type = T::GetType();
+		const auto it = _factories.find(type);
+		if (it == _factories.end())
+			_factories.insert({type, std::make_shared<cFactory<T>>(this)});
+	}
 }
